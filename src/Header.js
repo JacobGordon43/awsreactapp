125ce883f4da7731b5a3f80ba96188647c645de5
JacobGorodn43 <jacobg43@cox.net>
2022-10-13 21:26:48 -0700

    Worked on header component

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import { Router } from "express";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-font-awesome';
+import { faMagnifyinGlass } from '@fortawesome/free-solid-svg-icons'
+const Header = () =>{
+    return(
+        <header>
+            <nav>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/form">Form</Link>
+                    </li>
+                </ul>
+                <div className="search">
+                    <input type="text" id="searchbar">Search an item</input>
+                    <button type="submit" id="searchButton">faMagnifyinGlass</button>
+                </div>
+            </nav>
+        </header>
+    )
+}
